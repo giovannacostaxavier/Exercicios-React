15bfc3cfdbe7ca9d78e25373d3c5392f66a7de11
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-07-06 09:44:09 +0000

    Feat: props com map

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import CartaoFilme from "./components/CartaoFilme"
-
+import ListaFilmes from "./components/ListaFilmes"
 
 const App= ()=> {
-  
+  const filmes =[
+    { id: 1, titulo: "Crepúsculo", nota: 8, assistido: true },
+    { id: 2, titulo: "Matrix", nota: 9.5, assistido: true },
+    { id: 3, titulo: "Duna 2", nota: 9, assistido: false },
+  ]
   return (
    <div>
     <h2>Dados do projeto</h2>
 
-    <CartaoFilme 
-      titulo = "Crepúsculo"
-      nota = {8} 
-      assistido = {true}
-    
-    
+    <ListaFilmes
+      filmes={filmes}
     />
    </div>
 
---
+++ src/components/ListaFilmes.jsx
@@ -0,0 +1,17 @@
+
+
+const ListaFilmes=({filmes})=>{
+
+    const cartaoFilme = filmes.map((el)=>{
+        return(
+            <CartaoFilme key={el.id} titulo={el.titulo} nota={el.nota} assistido={el.assistido} />
+        )
+    })
+    return(
+        <div>
+            {cartaoFilme}
+        </div>
+    )
+}
+
+    export default ListaFilmes
