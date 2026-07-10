960c16cad1ed1ec9705750f2f021bc46b797ecc7
Giovanna <giovannacostaxavier@gmail.com>
2026-07-10 12:47:40 +0100

    Feat: exercicios de useEffect

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -8,6 +8,16 @@ const Contador= ()=> {
       console.log("componente carregou agora !");
     },[])
 
+    useEffect(()=>{
+        console.log(`O valor mudou para ${valor}`);
+    },[valor])
+
+
+    useEffect(()=>{
+        if(valor >= 5){
+          console.log("Você chegou no 5! 🎉");
+        }
+    },[valor])
   return (
    <div>
     <p>{valor}</p>
